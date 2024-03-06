ade68dc4f75e7473090dcb1f7829c60cf805648f
Jensen <jensen@huangyankai.com>
2024-03-06 11:20:36 +0800

    Counting Inversions

--- Extra/InversionCounting/src/InversionCounter.java
+++ Extra/InversionCounting/src/InversionCounter.java
@@ -1,7 +1,13 @@
 class InversionCounter {
 
     public static long countSwaps(int[] arr) {
-        return 0;
+        long inversions = 0;
+        for (int i = 0; i < arr.length; i++){
+            for (int j = i; j < arr.length; j++){
+                if (arr[i] > arr[j]) inversions++;
+            }
+        }
+        return inversions;
     }
 
     /**
@@ -10,6 +16,33 @@ class InversionCounter {
      * minimum amount of adjacent swaps needed to do so.
      */
     public static long mergeAndCount(int[] arr, int left1, int right1, int left2, int right2) {
-        return 0;
+        int[] result = new int[arr.length];
+        long swaps = 0;
+
+        int counterLeft = 0;
+        int counterRight = left2;
+
+        int resultCounter = 0;
+        while (counterLeft <= right1 && counterRight <= right2){
+            if (arr[counterRight] < arr[counterLeft]) {
+                result[resultCounter] = arr[counterRight];
+                swaps += counterRight-resultCounter;
+                counterRight++;
+            }
+            else{
+                result[resultCounter] = arr[counterLeft];
+                counterLeft++;
+            }
+            resultCounter++;
+        }
+        while (counterLeft <= right1){
+            result[resultCounter++] = arr[counterLeft++];
+        }
+        while (counterRight <= right2){
+            result[resultCounter++] = arr[counterRight++];
+        }
+        arr = result;
+        return swaps;
+
     }
 }
